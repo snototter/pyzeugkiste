84d94d18fb7df3192b17068bae067f7f46943aae
snototter <snototter@users.noreply.github.com>
2023-01-27 23:24:24 +0100

    update wzk

---
+++ src/pyzeugkiste/files/__init__.py
@@ -0,0 +1,18 @@
+from typing import Union
+from pathlib import Path
+#from pyzeugkiste._core import _files
+#__doc__ = _files.__doc__
+
+#TODO doc
+#TODO bindings
+#class AsciiFileIterator(object):
+#    def __init__(self, filename: Union[str, Path]) -> None:
+#        self._iterator = _files.AsciiFileIterator(filename)
+#    
+#    def __iter__(self):
+#        return self
+#
+#    def __next__(self) -> str:
+#        if self._iterator.is_done():
+#            raise StopIteration()
+#        return self._iterator.next()
