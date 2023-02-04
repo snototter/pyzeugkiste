48ffbb563f6d4ad40b9e6241c7e36c12d5a02caa
snototter <snototter@users.noreply.github.com>
2023-02-04 12:51:26 +0100

    initial config bindings

--- include/werkzeugkiste-bindings/config_bindings.h
+++ include/werkzeugkiste-bindings/config_bindings.h
@@ -7,10 +7,12 @@
 #include <werkzeugkiste/config/configuration.h>
 #include <werkzeugkiste/logging.h>
 
+#include <memory>
 #include <sstream>
 #include <stdexcept>
 #include <string_view>
-#include <memory>
+#include <utility>
+#include <vector>
 
 namespace wzkcfg = werkzeugkiste::config;
 
@@ -38,18 +40,75 @@ public:
     return cfg_->ToJSON();
   }
 
-  bool GetBoolean(const std::string &key) {//std::string_view key) {
+  bool Equals(const ConfigWrapper &other) const {
+    return cfg_->Equals(other.cfg_.get());
+  }
+
+  void SetBoolean(std::string_view key, bool value) {
+    return cfg_->SetBoolean(key, value);
+  }
+
+  bool GetBoolean(std::string_view key) const {//FIXME const
     return cfg_->GetBoolean(key);
   }
 
-//  bool GetBooleanOrDefault(std::string_view key, bool default_value) {
-//    return cfg_->GetBooleanOrDefault(key, default_value);
-//  }
+  bool GetBooleanOrDefault(std::string_view key, bool default_value) {
+    return cfg_->GetBooleanOrDefault(key, default_value);
+  }
 
-//  int32_t GetInteger32(std::string_view key) {
-//    return cfg_->GetInteger32(key);
-//  }
+  void SetInteger32(std::string_view key, int32_t value) {
+    return cfg_->SetInteger32(key, value);
+  }
 
+  int32_t GetInteger32(std::string_view key) {
+    return cfg_->GetInteger32(key);
+  }
+
+  int32_t GetInteger32OrDefault(std::string_view key, int32_t default_value) {
+    return cfg_->GetInteger32OrDefault(key, default_value);
+  }
+
+  void SetInteger64(std::string_view key, int64_t value) {
+    return cfg_->SetInteger64(key, value);
+  }
+
+  int64_t GetInteger64(std::string_view key) {
+    return cfg_->GetInteger64(key);
+  }
+
+  int64_t GetInteger64OrDefault(std::string_view key, int64_t default_value) {
+    return cfg_->GetInteger64OrDefault(key, default_value);
+  }
+
+  void SetDouble(std::string_view key, double value) {
+    return cfg_->SetDouble(key, value);
+  }
+
+  double GetDouble(std::string_view key) {
+    return cfg_->GetDouble(key);
+  }
+
+  double GetDoubleOrDefault(std::string_view key, double default_value) {
+    return cfg_->GetDoubleOrDefault(key, default_value);
+  }
+
+  void SetString(std::string_view key, std::string_view value) {
+    return cfg_->SetString(key, value);
+  }
+
+  std::string GetString(std::string_view key) {
+    return cfg_->GetString(key);
+  }
+
+  std::string GetStringOrDefault(std::string_view key, std::string_view default_value) {
+    return cfg_->GetStringOrDefault(key, default_value);
+  }
+
+
+  // Special functions
+  bool ReplacePlaceholders(const std::vector<std::pair<std::string_view, std::string_view>> &replacements) {
+    return cfg_->ReplaceStringPlaceholders(replacements);
+  }
 
 private:
   std::unique_ptr<werkzeugkiste::config::Configuration> cfg_{};
@@ -65,32 +124,94 @@ inline void RegisterConfiguration(pybind11::module &m) {
   pybind11::class_<ConfigWrapper> cfg(m, "Configuration", doc.str().c_str());
 
   // Loading a configuration
+  std::ostringstream().swap(doc);
+  doc << "Returns a :class:`~" << module_name
+      << ".Configuration loaded from the given TOML file.";
   cfg.def_static("load_toml_file", &ConfigWrapper::LoadTOMLFile,
-                 "TODO", pybind11::arg("filename"));
+                 doc.str().c_str(), pybind11::arg("filename"));
+
+  std::ostringstream().swap(doc);
+  doc << "Returns a :class:`~" << module_name
+      << ".Configuration loaded from the given TOML string.";
   cfg.def_static("load_toml_string", &ConfigWrapper::LoadTOMLString,
-                 "TODO", pybind11::arg("toml_str"));
+                 doc.str().c_str(), pybind11::arg("toml_str"));
 
   // Serializing
   cfg.def("to_toml", &ConfigWrapper::ToTOMLString,
-          "TODO");
+          "Returns a formatted TOML representation of this configuration.");
 
-  cfg.def("to_jsong", &ConfigWrapper::ToJSONString,
-          "TODO");
-  // Getting/Setting scalars
+  cfg.def("to_json", &ConfigWrapper::ToJSONString,
+          "Returns a formatted JSON representation of this configuration.");
+
+
+  // Getting/setting scalars
   // TODO
+  cfg.def("set_bool", &ConfigWrapper::SetBoolean,
+          "TODO", pybind11::arg("key"), pybind11::arg("value"));
   cfg.def("get_bool", &ConfigWrapper::GetBoolean,
           "TODO", pybind11::arg("key"));
-//  cfg.def("get_bool_or", &ConfigWrapper::GetBooleanOrDefault,
-//          "TODO", pybind11::arg("key"), pybind11::arg("default"));
+  cfg.def("get_bool_or", &ConfigWrapper::GetBooleanOrDefault,
+          "TODO", pybind11::arg("key"), pybind11::arg("default_value"));
 
-//  cfg.def("get_int32", &ConfigWrapper::GetInteger32,
-//          "TODO", pybind11::arg("key"));
+
+  cfg.def("set_int32", &ConfigWrapper::SetInteger32,
+          "TODO", pybind11::arg("key"), pybind11::arg("value"));
+  cfg.def("get_int32", &ConfigWrapper::GetInteger32,
+          "TODO", pybind11::arg("key"));
+  cfg.def("get_int32_or", &ConfigWrapper::GetInteger32OrDefault,
+          "TODO", pybind11::arg("key"), pybind11::arg("default_value"));
+
+
+  cfg.def("set_int64", &ConfigWrapper::SetInteger64,
+          "TODO", pybind11::arg("key"), pybind11::arg("value"));
+  cfg.def("get_int64", &ConfigWrapper::GetInteger64,
+          "TODO", pybind11::arg("key"));
+  cfg.def("get_int64_or", &ConfigWrapper::GetInteger64OrDefault,
+          "TODO", pybind11::arg("key"), pybind11::arg("default_value"));
+
+
+  cfg.def("set_double", &ConfigWrapper::SetDouble,
+          "TODO", pybind11::arg("key"), pybind11::arg("value"));
+  cfg.def("get_double", &ConfigWrapper::GetDouble,
+          "TODO double-precision", pybind11::arg("key"));
+  cfg.def("get_double_or", &ConfigWrapper::GetDoubleOrDefault,
+          "TODO double-precision", pybind11::arg("key"), pybind11::arg("default_value"));
+
+  cfg.def("set_str", &ConfigWrapper::SetString,
+          "TODO", pybind11::arg("key"), pybind11::arg("value"));
+  cfg.def("get_str", &ConfigWrapper::GetString,
+          "TODO", pybind11::arg("key"));
+  cfg.def("get_str_or", &ConfigWrapper::GetStringOrDefault,
+          "TODO", pybind11::arg("key"), pybind11::arg("default_value"));
 
   // Getting/Setting lists/tuples/pairs
   // TODO
 
   // Special utils
-  // TODO load nested, replace placeholders
+  // TODO load nested, replace placeholders, adjust paths
+  cfg.def("replace_placeholders", &ConfigWrapper::ReplacePlaceholders,
+          "TODO doc + example [('search', 'replace'), ('e', '')]"
+"TODO watch out for side effects (replacements will be applied in order)", pybind11::arg("placeholders"));
+
+
+  // Equality checks
+  cfg.def(
+      "__eq__", [](const ConfigWrapper &a, const ConfigWrapper &b) -> bool {
+          return a.Equals(b);
+      },
+      "Returns `True` if both configs contain the exact same parameters (keys, types and values).",
+      pybind11::arg("other"));
+
+  std::ostringstream().swap(doc);
+  doc << "Checks for inequality, see :meth:`~." << module_name
+      << ".Configuration.__eq__` for details.";
+  cfg.def(
+      "__ne__", [](const ConfigWrapper &a, const ConfigWrapper &b) -> bool {
+          return !a.Equals(b);
+      },
+      doc.str().c_str(), pybind11::arg("other"));
+
+  //TODO __str__ and __repr__, e.g. (x 1st level keys, y parameters in total)
 }
 }  // namespace werkzeugkiste::bindings
 
---
+++ tests/test_config.py
@@ -0,0 +1,84 @@
+import pytest
+from pyzk import config
+
+def test_scalars():
+    cfg = config.Configuration.load_toml_string("""
+        int = 23
+        flt = 1.5
+        str = "value"
+        bool = false
+        """)
+
+    ###########################################################################
+    # Boolean
+    assert not cfg.get_bool('bool')
+
+    # -- Implicit type conversion is not allowed
+    with pytest.raises(RuntimeError):
+        cfg.get_bool('int')
+    with pytest.raises(RuntimeError):
+        cfg.get_bool('flt')
+    with pytest.raises(RuntimeError):
+        cfg.get_bool('str')
+
+    # -- Raises an error if the key cannot be found
+    with pytest.raises(RuntimeError):
+        cfg.get_bool('nested.scalars.b')
+
+    # -- Solution 1) Set the missing property
+    cfg.set_bool('nested.scalars.bool', value = True)
+    assert cfg.get_bool('nested.scalars.bool')
+
+    # -- Solution 2) Explicitly request a default value
+    assert cfg.get_bool_or("no-such-key", default_value = True)
+    assert not cfg.get_bool_or("no-such-key", False)
+
+    ###########################################################################
+    # Strings
+    assert cfg.get_str('str') == 'value'
+
+    with pytest.raises(RuntimeError):
+        cfg.get_str('nested.scalars.bool')
+
+    with pytest.raises(RuntimeError):
+        cfg.get_str('nested.scalars.str')
+ 
+    cfg.set_str('nested.scalars.str', 'frobmorten')
+    assert cfg.get_str("nested.scalars.str") == 'frobmorten'
+
+    assert cfg.get_bool_or("no-such-key", True)
+    assert not cfg.get_bool_or("no-such-key", False)
+
+def test_representations():
+    #TODO load an extensive toml example instead!
+    cfg = config.Configuration.load_toml_string("""
+        int = 23
+        flt = 1.5
+        str = "value"
+        bool = false
+        """)
+    other = config.Configuration.load_toml_string(cfg.to_toml())
+    assert cfg == other
+
+    with pytest.raises(RuntimeError):
+        config.Configuration.load_toml_string(cfg.to_json())
+
+
+def test_placeholders():
+    cfg = config.Configuration.load_toml_string("""
+        str1 = 'value'
+        str2 = '[%REP%]'
+
+        [tbl]
+        str1 = 'value %REP%'
+        str2 = '123'
+        str3 = 'key'
+        """)
+
+    cfg.replace_placeholders([('%REP%', '...'), ('e', '')])
+    assert cfg.get_str('str1') == 'valu'
+    assert cfg.get_str('str2') == '[...]'
+    assert cfg.get_str('tbl.str1') == 'valu ...'
+    assert cfg.get_str('tbl.str2') == '123'
+    assert cfg.get_str('tbl.str3') == 'ky'
+
